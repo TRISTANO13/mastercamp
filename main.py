f59afcac87c8841861a834aef89271efafc2ea77
Luc205299 <94066059+Luc205299@users.noreply.github.com>
2024-06-12 11:47:11 +0200

    dtill be ignored, and an empty message aborts the commit.

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from tkinter import Tk
 from loginWin import LoginWindow
+from database import*
+
 
 if __name__ == "__main__":
+    # Afficher les utilisateurs dans le terminal
+    print_users()
+
+    # Supprimer un utilisateur
+    username_to_delete = ""
+    delete_user(username_to_delete)
+    print(f"User '{username_to_delete}' deleted.")
+
+    # Afficher les utilisateurs après suppression
+    print_users()
+
     root = Tk()
     app = LoginWindow(root)
     root.mainloop()
